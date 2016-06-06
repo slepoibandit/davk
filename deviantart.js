5afeaa293a615b835d72b4ba2de46d42a7f699bb
test <test>
2016-06-06 23:30:48 +0300

    Get working version of token update function

--- deviantart.js
+++ deviantart.js
@@ -5,12 +5,6 @@ var headers = {
   'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
 };
 
-var testurl = {
-  protocol: 'https:',
-  hostname: 'www.deviantart.com',
-  path: '/oauth2/token?client_id=4787&client_secret=36b25fc8db5d33504ef10e241679d6e0&grant_type=client_credentials',
-  headers: headers
-};
 var deviantArt = {
   access_token: '',
   client_id: 4787,
@@ -26,14 +20,20 @@ var deviantArt = {
       client_secret: deviantArt.client_secret,
       grant_type: 'client_credentials'
     };
-    https.get(testurl, function(response){
+    var options = {
+      protocol: deviantArt.urlObject.protocol,
+      hostname: deviantArt.urlObject.hostname,
+      path: url.parse(url.format(deviantArt.urlObject)).path,
+      headers: headers
+    };
+    https.get(options, function(response){
       var data = '';
-      console.log(testurl);
       response.on('data', function(chunk){
         data += chunk;
       });
       response.on('end', function(){
         deviantArt.access_token = data;
+        console.log(data);
       });
     }).on('error', function(error){
       console.error('Error updating token: ' + error);
